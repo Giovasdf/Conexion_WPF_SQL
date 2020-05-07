24a89dff4a0c8d3064eee4d63015bd487f013361
Giovanni Molina <giov.molina@alumnos.duoc.cl>
2020-05-07 11:53:44 -0400

    first commit
    proyecto de conexion a sql server

---
+++ CreadorUsuariosTest/LibreriaClases/EjecutarQuery.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaClases
+{
+    public class EjecutarQuery
+    {
+
+        static string connectionString = @"Server = DESKTOP-6N3RFB9\SQLEXPRESS; Database=Usuarios;Trusted_Connection=True";
+        public static void SqlCommandPrepareEx( string nombre,string apellido)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(null, connection);
+
+                // Create and prepare an SQL statement.
+                command.CommandText = "INSERT INTO usuario (nombre, apellido) " + "VALUES (@nombre, @apellido)";             
+                command.Parameters.AddWithValue("@nombre", nombre);
+                command.Parameters.AddWithValue("@apellido", apellido);
+
+                try
+                {
+                    connection.Open();
+                    Int32 rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine("RowsAffected: {0}", rowsAffected);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                connection.Close();
+            }
+        }
+        public static void SqlCommandPrepareEx(string nombre)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(null, connection);
+
+                // Create and prepare an SQL statement.
+                command.CommandText = "INSERT INTO usuario (nombre) " + "VALUES (@nombre)";
+                command.Parameters.AddWithValue("@nombre", nombre);
+
+                try
+                {
+                    connection.Open();
+                    Int32 rowsAffected = command.ExecuteNonQuery();
+                    Console.WriteLine("RowsAffected: {0}", rowsAffected);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
