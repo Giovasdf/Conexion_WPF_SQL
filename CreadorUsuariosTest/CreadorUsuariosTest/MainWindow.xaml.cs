6fbe9de734fb7e03c7afc182b09e8fa3193342ca
Giovanni Molina <giov.molina@alumnos.duoc.cl>
2020-05-27 11:07:23 -0400

    CalculateDate
    Added function to calculate date

--- CreadorUsuariosTest/CreadorUsuariosTest/MainWindow.xaml.cs
+++ CreadorUsuariosTest/CreadorUsuariosTest/MainWindow.xaml.cs
@@ -56,5 +56,23 @@ namespace CreadorUsuariosTest
                 MessageBox.Show("No se encontro ningun usuario", "Alerta", MessageBoxButton.OK, MessageBoxImage.Asterisk);
             }
         }
+
+        private void btnCalcularFecha_Click(object sender, RoutedEventArgs e)
+        {
+            //Inicio Vigencia
+            DateTime dt1 = DP1.SelectedDate.Value;
+            //Fin Vigencia
+            //DateTime dt2 = DP2.SelectedDate.Value;
+            DateTime dt2 = new DateTime(dt1.Year+1, dt1.Month, dt1.Day);
+
+            DP2.SelectedDate = dt2;
+
+            TimeSpan tspan = dt2 - dt1;
+
+            int diferenciaDias = tspan.Days;
+
+            MessageBox.Show($"Diferencia Fecha {diferenciaDias}", "Resultado", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+        }
     }
 }
